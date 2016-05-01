8529d7ba3dfe1a5e23b24fa291fa7ff71a25f683
Hardik Kaji <hardikkaji@gmail.com>
2016-05-01 17:48:01 +0530

    installed new package and added main index file.

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,21 @@
 // importing express
 var express = require('express');
+var bodyParser = require('body-parser');
 
 //assigning express to app varaible
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //listening to default page.
 app.get('/', function(request, response) {
-	response.send('Hello World');
+	response.send('Hello World.!');
 });
 
 //listening to port and when port is ready to serve request logging message.
