d38004b8c94a7b8033202250440edefaf53e3764
Hardik Kaji <hardikkaji@gmail.com>
2016-09-18 20:04:47 +0530

    fixed

--- src/client/app.js
+++ src/client/app.js
@@ -1,16 +1,16 @@
-(function(angular) {
+(function (angular) {
 
-    var todoApp = angular.module('todo.app', []);
-    todoApp.directive('todoSvg', function() {
-        // directive construct
-        return {
-            restrict: 'E',
-            scope: {
-                'type': '@type'
-            },
-            templateUrl: 'partials/todo-svg.html'
-        };
-    });
+	var todoApp = angular.module('todo.app', []);
+	todoApp.directive('todoSvg', function () {
+		// directive construct
+		return {
+			restrict: 'E',
+			scope: {
+				'type': '@type'
+			},
+			templateUrl: 'partials/todo-svg.html'
+		};
+	});
 
-    angular.bootstrap(document, ['todo.app']);
+	angular.bootstrap(document, ['todo.app']);
 })(window.angular);
