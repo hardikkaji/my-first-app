50e3de979a0088d81013e5ae78feda55c3fc289c
Hardik Kaji <hardikkaji@gmail.com>
2016-09-18 19:42:35 +0530

    Todo app integrated.

---
+++ src/client/app.js
@@ -0,0 +1,16 @@
+(function(angular) {
+
+    var todoApp = angular.module('todo.app', []);
+    todoApp.directive('todoSvg', function() {
+        // directive construct
+        return {
+            restrict: 'E',
+            scope: {
+                'type': '@type'
+            },
+            templateUrl: 'partials/todo-svg.html'
+        };
+    });
+
+    angular.bootstrap(document, ['todo.app']);
+})(window.angular);
