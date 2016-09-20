37b53f8d9b70dd0cf19a030f7be423b6853eff37
Hardik Kaji <hardik.kaji@mastercard.com>
2016-09-20 17:46:16 +0530

    browserSync integrated;

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var config = require('./gulp.config')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('serve', serve);
 
@@ -8,11 +10,13 @@ gulp.task('serve', serve);
 
 function serve() {
 	startNodemon(getNodemonConfig());
+	gulp.watch('./src/**/*.*', browserSync.reload);
 }
 
 function getNodemonConfig() {
 	return {
 		script: config.server + 'app.js',
+		watch: ['./src/**/*.*'],
 		ext: 'html js'
 	};
 }
@@ -21,9 +25,17 @@ function startNodemon(config) {
 	nodemon(config)
 		.on('start', function() {
 			console.log('**** gulp-nodemon started.!');
+			browserSync({
+				proxy: "localhost:3000",  // local node app address
+				port: 5000,  // use *different* port than above
+				notify: true
+			});
 		})
 		.on('restart', function() {
 			console.log('**** Server restarted.!');
+			setTimeout(function () {
+				reload({ stream: false });
+			}, 1000);
 		});
 
 }
